15b6b79da8eabc390c6823c045e93a9db550c8d7
Kevin de Bie <kevin.de.bie@maxserv.com>
2023-05-19 15:18:26 +0200

    Fixed paths

--- dist/bundle.js
+++ dist/bundle.js
@@ -3118,8 +3118,8 @@ var globals = {
     countries: [],
     regions: [],
     isLoading: false,
-    chevronDown: '/src/icons/chevron-down.svg#chevron-down',
-    arrowLeft: '/src/icons/arrow-left.svg#arrow-left',
+    chevronDown: 'src/icons/chevron-down.svg#chevron-down',
+    arrowLeft: 'src/icons/arrow-left.svg#arrow-left',
     notFound: 'Not found',
 
     init() {
@@ -3249,7 +3249,7 @@ class Header extends HTMLElement {
         <header class="bg-white py-8 shadow-md shadow-gray-100 dark:bg-dark-blue">
             <div class="max-w-none flex justify-between items-center px-4 md:px-8 lg:max-w-[1350px] lg:mx-auto">
                 <span class="mb-0 text-xl md:text-2xl font-bold dark:text-white">
-                    <a :href="window.location.href.includes('?country') ? '../index.html' : '/${rootPath}/index.html'">Where in the world?</a>
+                    <a :href="window.location.href.includes('?country') ? './index.html' : '/${rootPath}/index.html'">Where in the world?</a>
                 </span>
                 <button @click="$store.darkMode = ! $store.darkMode" 
                         class="text-black font-semibold text-lg inline-flex gap-3 items-center dark:text-white">
--- src/js/header.js
+++ src/js/header.js
@@ -10,7 +10,7 @@ class Header extends HTMLElement {
         <header class="bg-white py-8 shadow-md shadow-gray-100 dark:bg-dark-blue">
             <div class="max-w-none flex justify-between items-center px-4 md:px-8 lg:max-w-[1350px] lg:mx-auto">
                 <span class="mb-0 text-xl md:text-2xl font-bold dark:text-white">
-                    <a :href="window.location.href.includes('?country') ? '../index.html' : '/${rootPath}/index.html'">Where in the world?</a>
+                    <a :href="window.location.href.includes('?country') ? './index.html' : '/${rootPath}/index.html'">Where in the world?</a>
                 </span>
                 <button @click="$store.darkMode = ! $store.darkMode" 
                         class="text-black font-semibold text-lg inline-flex gap-3 items-center dark:text-white">
